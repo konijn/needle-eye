70ef8e6d0aa2db19b89d017142f34455d14eeb37
Konijn <konijn@gmail.com>
2019-05-07 13:47:31 -0400

    Comments, spell fix, and 5 new enhancments

--- idle_portal/base.js
+++ idle_portal/base.js
@@ -1,10 +1,50 @@
 /*
+  Idle portal, UI
+  Inspiration: various idle games
+*/
+/*jslint vars: true, browser: true, devel: true, esversion: 6 */
+/*jshint esversion: 6, strict: true, futurehostile: true, maxerr:200, -W097  */
+
+/*
   Fortune favors the bold
 */
+"use strict";
 
-/*Cut, a chanaible splice of length 1*/
+//Cut, a chainable splice of length 1
 Array.prototype.cut = function arrayCut(index){
   const entry = this[index];
   this.splice(index,1);
   return entry;
+};
+
+//
+Array.prototype.query = function arrayQuery(query){
+  for(const o of this){
+    if(o[query.key] == query.value){
+      return o;
+    }
+  }
+};
+
+//Pick a random entry of an array
+Array.prototype.pick = function arrayPick(){
+  return this[Math.floor(Math.random() * this.length)];
+};
+
+//Pick a random value of an object
+Object.prototype.pick = function arrayPick(){
+  return this[Object.keys(this).pick()];
+};
+
+
+//Prefix something with either a or an
+String.prototype.an = function stringAn(){
+  const c = this[0];
+  return (("aeyiou".includes(c))?"an ":"a ") + this;
+};
+
+//Prefix something with either A or An
+String.prototype.An = function stringAN(){
+  const c = this[0];
+  return (("aeyiou".includes(c))?"An ":"A ") + this;
 };
