77d13ffcdfb7f429668735fb678efd8f44e49f6c
Konijn <konijn@gmail.com>
2019-05-07 13:46:15 -0400

    May the RNG be with you

---
+++ idle_portal/odds.js
@@ -0,0 +1,19 @@
+/*
+  Idle portal, UI
+  Inspiration: various idle games
+*/
+/*jslint vars: true, browser: true, devel: true, esversion: 6 */
+/*jshint esversion: 6, strict: true, futurehostile: true, maxerr:200, -W097  */
+/* jshint -W093 */
+/*globals data*/
+
+"use strict";
+
+const odds = {
+
+  d100: function d100(){
+    return Math.floor(Math.random() * 100) + 1;
+  }
+
+
+};
