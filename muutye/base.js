4301253641df33ca8635e56b9366c3345b1ed917
Konijn <konijn@gmail.com>
2020-06-07 19:38:32 +0200

    Add Array.has and Array.drop

--- muutye/base.js
+++ muutye/base.js
@@ -1,5 +1,7 @@
 /*jshint esversion: 6 */
 
+"Use strict";
+
 String.prototype.textNodeTrim = function textNodeTrim(){
 
 	let s = this.replace(/\t/g, '');
@@ -16,5 +18,13 @@ String.prototype.word = function stringWord(){
 	///\s/ => any white space
 	///^[a-z0-9]+$/i => only alphanumeric
 
-		return this.split(/\s/).shift().replace(/[^a-z0-9]/gi,'');
+	return this.split(/\s/).shift().replace(/[^a-z0-9]/gi,'');
+};
+
+Array.prototype.has = function arrayHas(element){
+	return this.includes(element);
+};
+
+Array.prototype.drop = function arrayDrop(element){
+	return this.filter(e => e != element);
 };
