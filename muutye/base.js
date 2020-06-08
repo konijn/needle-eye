3112a08d9d975e88ba8f649858cd7b7d86003a97
Konijn <konijn@gmail.com>
2020-06-08 21:42:48 +0200

    alignLeft, numberedList, numberedKeyValues

--- muutye/base.js
+++ muutye/base.js
@@ -14,12 +14,28 @@ String.prototype.textNodeTrim = function textNodeTrim(){
 	return s;
 };
 
-String.prototype.word = function stringWord(){
+String.prototype.firstWord = function stringWord(){
 	///\s/ => any white space
 	///^[a-z0-9]+$/i => only alphanumeric
-
 	return this.split(/\s/).shift().replace(/[^a-z0-9]/gi,'');
 };
+
+String.prototype.capitalize = function stringCapitalize() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+String.prototype.decapitalize = function stringDecapitalize() {
+  return this.charAt(0).toLowerCase() + this.slice(1);
+};
+
+String.prototype.alignLeft = function stringAlignLeft(length, filler){
+	filler = filler || ' ';
+	return (this + filler.repeat(length - this.length)).substring(0, length);
+};
+
+Number.prototype.alignLeft = function numberAlignLeft(length, filler){
+	return (this+'').alignLeft(length, filler);
+}
 
 Array.prototype.has = function arrayHas(element){
 	return this.includes(element);
@@ -28,3 +44,15 @@ Array.prototype.has = function arrayHas(element){
 Array.prototype.drop = function arrayDrop(element){
 	return this.filter(e => e != element);
 };
+
+Array.prototype.numberedList = function numberedList(){
+	const idLength = (this.length + " ").length;
+	return this.map((content,index)=>`${(index+1+'').alignLeft(idLength)}${content}`).join('\n');
+};
+
+Object.prototype.numberedKeyValues = function numberedKeyValues(){
+	const keys = Object.keys(this);
+	const keysLength = Math.max(...keys.map(key => key.length));
+	const idLength = (keys.length+'').length;
+	return keys.map((key,index)=>`${index.alignLeft(idLength)} ${key.alignLeft(keysLength)} ${this[key]}`).join('\n');
+}
