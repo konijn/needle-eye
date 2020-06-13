5fd947587658604c895acc43f04ddab1cdd51c23
Konijn <konijn@gmail.com>
2020-06-13 23:21:52 +0200

    Silly support, system support, and much (much) cleaner approach to routines

--- muutye/ut.js
+++ muutye/ut.js
@@ -1,5 +1,13 @@
-/*jshint esversion: 6 */
+/*jshint esversion: 6, node: true */
 
+"use strict";
+
+const DEBUG = 0;
+const WARNING = 4;
+const SUCCESS = 6;
+const ERROR = 8;
+const CATASTROPHE = 16;
+const CRITICAL = 16;
 
 const log = require('./log.js').log;
 
@@ -11,7 +19,11 @@ function expectEquals(testCase, expected, actual){
     console.log(testCase, 'Passed'.green, `(${actual})`);
   }else{
     console.log(testCase, 'Failed'.red);
-    console.log('Expected:'.yellow, expected, '\nActual  :'.yellow, actual);
+    console.log('Expected:'.yellow, expected, expected.length);
+    console.log('Actual  :'.yellow, actual, actual.length);
+    for(const c of actual){
+      console.log(`${c} -> ${c.charCodeAt(0)}`);
+    }
   }
   return success;
 }
@@ -23,35 +35,31 @@ function updateTally(isSuccess){
   }
 }
 
-function runTest(categoryName, expectedResult){
-  //console.log(`Expected: ${expectedResult}`);
-  const category = routines.findCategory(categoryName);
-  //console.log(routines);
-  if(!category){
-    log(CRITICAL, `Could not find category ${categoryName}`);
-  }
-  if(!category.template){
-    log(CRITICAL, `Category ${categoryName} does not have a template`);
-  }
-  const out = routines.runNode(category.template);
-  updateTally(expectEquals(`${tally.total+1}) ${categoryName}`, expectedResult, out));
-}
-
 
 module.exports = {
 
-  routines: undefined,
+  run : function run(routines){
 
-  setRoutines: function setRoutines(functions){
-    routines = functions;
-  },
+    function runTest(categoryName, expectedResult){
+      //console.log(`Expected: ${expectedResult}`);
+      const category = routines.findCategory(categoryName);
+      //console.log(routines);
+      if(!category){
+        log(CRITICAL, `Could not find test category ${categoryName}`);
+      }
+      if(!category.template){
+        log(CRITICAL, `Category ${categoryName} does not have a template`);
+      }
+      const out = routines.runNode(category.template);
+      updateTally(expectEquals(`${tally.total+1}) ${categoryName}`, expectedResult, out));
+    }
 
-  run : function run(){
     tally = {
       total: 0,
       success: 0
     };
     console.log('I ran');
+
     routines.loadAIML('ut.aiml');
 
     runTest('Test WhoAmI', 'My name is Muutye');
@@ -116,11 +124,18 @@ module.exports = {
     runTest('test star end the credits roll', 'End star matched: the credits roll');
     runTest('test star having multiple stars in a pattern makes me extremely happy', 'Multiple stars matched: having, stars in a pattern, extremely happy');
 
-    runTest('test system', 'The system says Nice Try!');
-    
+    //runTest('test system', 'The system says You wish..');
+    runTest('test system', 'The system says hello');
+
     runTest('test plurals 1', 'The plural of house is houses, of fish is fishes');
     runTest('test plurals 2', 'The plural of memory is memories, of calf is calves');
 
+    runTest('test create new silly', 'Sure!');
+    runTest('test reload sillies', 'I reloaded the sillies.');
+    runTest('test new silly', 'Run!');
+    runTest('test drop silly', 'I wont respond to test any more.');
+    runTest('test drop dropped silly', 'I\'ve not heard that one before.');
+    runTest('test dropped silly', 'Got ya');
 
     //Colorize the state of affairs
     if(tally.total == tally.success){
