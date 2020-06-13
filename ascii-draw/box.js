74a2ae7a4f1c32a715bb50ac95c6a9d8659cc70a
Konijn <konijn@gmail.com>
2020-06-13 19:41:43 +0200

    Spaces, jshint, no more anonymous double arrow functions

--- ascii-draw/box.js
+++ ascii-draw/box.js
@@ -1,36 +1,38 @@
+/*jshint -W085, esversion: 6  */
+
 function Box( cell1 , cell2 )
 { //Constructor for box instances by using 2 cells
   //Find the upper left corner
-  var x1 = Math.min( cell1.x , cell2.x );
-  var y1 = Math.min( cell1.y , cell2.y );
+  const x1 = Math.min( cell1.x , cell2.x );
+  const y1 = Math.min( cell1.y , cell2.y );
   //Find the bottom right corner
-  var x2 = Math.max( cell1.x , cell2.x );
-  var y2 = Math.max( cell1.y , cell2.y );    
+  const x2 = Math.max( cell1.x , cell2.x );
+  const y2 = Math.max( cell1.y , cell2.y );
   //Set the normalized from & to
   this.from = {  x : x1, y : y1 };
-  this.to   = {  x : x2, y : y2 };   
+  this.to   = {  x : x2, y : y2 };
 }
 
 
-Box.prototype.each = function( f )
+Box.prototype.each = function boxEach( f )
 { //Loop over each cell, call f with an x,y object
   with (this)
-    for( var x = from.x ; x <= to.x ; x++ )
-      for( var y = from.y ; y <= to.y ; y++ )
+    for( let x = from.x ; x <= to.x ; x++ )
+      for( let y = from.y ; y <= to.y ; y++ )
         f( { x : x , y : y } );
-}
+};
 
-Box.prototype.eachRow = function( f )
+Box.prototype.eachRow = function boxEachRow( f )
 { //Loop over each row, call f
   with (this)
-    for( var y = from.y ; y <= to.y ; y++ )
+    for( let y = from.y ; y <= to.y ; y++ )
       f( y );
-}
+};
 
 //Loop over each column, call f
-Box.prototype.eachColumn = function( f )
+Box.prototype.eachColumn = function boxEachColumn( f )
 { //Loop over each row, call f
   with (this)
-    for( var x = from.x ; x <= to.x ; x++ )
+    for( let x = from.x ; x <= to.x ; x++ )
       f( x );
-}
+};
